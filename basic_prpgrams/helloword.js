4c99d7ff187cd7cfd9e0a6add00e6acd8b302337
unknown <pavankumarbr17@gmail.com>
2026-01-14 08:23:15 +0530

    1st

---
+++ basic_prpgrams/helloword.js
@@ -0,0 +1,26 @@
+console.log("helloworld")
+
+//comments are written like this 
+/* multi line 
+   comment*/
+
+
+//declaig variable - no need to explicitly mention the type of 
+let a = 2 
+var b = 3
+console.log(a+b)
+
+
+// // for loop 
+// let k =0 
+// for(let k=0;k<=10;k=k+2)
+// {
+//    console.log(k)
+// }
+
+
+//arrays
+var marks = [10,20,30,40]
+
+console.log(marks[0])
+console.log(marks.length)
